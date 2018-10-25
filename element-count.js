fa65924f0f4d5aa9f405da6ea4b8a642e09b5a28
Marcy Silverman <marcysilve225@gmail.com>
2018-10-24 17:06:32 -0700

    final version

--- element-count.js
+++ element-count.js
@@ -2,26 +2,20 @@
 'use strict'
 
 function tryQuerySelector() {
-    var sectionInputs = document.querySelectorAll('input[name="section"]');
-
+    var sectionInputs = document.querySelectorAll('input[name="section"]:checked');
+    var totalCount = document.getElementById("total-count"); 
+    var msg = " "; 
+    
     for (var i=0; i < sectionInputs.length; i++) {
-        var input = sectionInputs[i]; 
-        console.log("bananas", input.value, input.id, input.checked);
+        
+        var input = sectionInputs[i].value; 
+        console.log(input);
+        var total = document.querySelectorAll(input); 
+        var count = total.length;
+        console.log(count);
+        
+         msg += input + ' - ' + count + "<br>";
+        
     }
-
-    var selectedSectionInput = document.querySelector('input[name="section"]:checked');
-
-    if(selectedSectionInput) {
-        console.log("selected input value", selectedSectionInput.value); 
-    }
-
-    else {
-        console.log('no section selected');
-    }
-
-    var selectedAllInput = document.querySelector('input checked'); 
-
-    if (selectedAllInput) {
-        console.log("all selected input", selectedAllInput.value); 
-    }
-} 
+    totalCount.innerHTML = msg; 
+}
